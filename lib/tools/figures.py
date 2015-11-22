e63aad17bf200b4f86baf6e7af34bf0e152995ff
JulianStirling <julian@julianstirling.co.uk>
2015-11-22 13:41:56 -0500

    Figure colours correct on right click. Elipse error catching removed for sensible fix

--- lib/tools/figures.py
+++ lib/tools/figures.py
@@ -31,9 +31,15 @@ class RectangleTool(DragAndDropTool):
         w = self.final_x - self.initial_x
         h = self.final_y - self.initial_y
         context.rectangle(self.initial_x, self.initial_y, w, h)
-        self.use_secondary_color(context)
+        if self.m_button==3:
+            self.use_primary_color(context)
+        else:
+            self.use_secondary_color(context)
         context.fill_preserve()
-        self.use_primary_color(context)
+        if self.m_button==3:
+            self.use_secondary_color(context)
+        else:
+            self.use_primary_color(context)
         context.stroke()
 
 
@@ -46,9 +52,15 @@ class RoundedRectangleTool(DragAndDropTool):
         w = self.final_x - self.initial_x
         h = self.final_y - self.initial_y
         context.rectangle(self.initial_x, self.initial_y, w, h)
-        self.use_secondary_color(context)
+        if self.m_button==3:
+            self.use_primary_color(context)
+        else:
+            self.use_secondary_color(context)
         context.fill_preserve()
-        self.use_primary_color(context)
+        if self.m_button==3:
+            self.use_secondary_color(context)
+        else:
+            self.use_primary_color(context)
         context.save()
         context.set_line_width(5)
         context.set_line_join(cairo.LINE_JOIN_ROUND)
@@ -64,21 +76,32 @@ class EllipseTool(DragAndDropTool):
 
         w = self.final_x - self.initial_x
         h = self.final_y - self.initial_y
-        context.save()
-        context.translate(self.initial_x + w/2., self.initial_y + h/2.)
-        try:
-            # This statements throw an exception, but they do not affect the tool's
-            # behaviour. Thus, I hide them from the user.
+        
+        if w!=0 and h !=0:
+            context.save()
+            context.translate(self.initial_x + w/2., self.initial_y + h/2.)
             context.scale(w/2., h/2.)
             context.arc(0., 0., 1., 0., 2 * math.pi)
-            self.use_secondary_color(context)
+            if self.m_button==3:
+                self.use_primary_color(context)
+            else:
+                self.use_secondary_color(context)
             context.fill_preserve()
             context.restore()
-            self.use_primary_color(context)
+            if self.m_button==3:
+                self.use_secondary_color(context)
+            else:
+                self.use_primary_color(context)
+            #context.set_antialias(cairo.ANTIALIAS_NONE)
+        else:
+            if self.m_button==3:
+                self.use_secondary_color(context)
+            else:
+                self.use_primary_color(context)
             context.set_antialias(cairo.ANTIALIAS_NONE)
-            context.stroke()
-        except cairo.Error:
-            pass
+            context.move_to(self.initial_x, self.initial_y)
+            context.line_to(self.final_x, self.final_y)
+        context.stroke()
 
 class RectangleSelectTool(DragAndDropTool):
     name = 'RectSelect'
